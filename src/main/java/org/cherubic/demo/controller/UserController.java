60aa0039d70d6077881128a2d5989ccd1171ec9e
cutiechi <chi@cutie.ooo>
2018-08-31 18:55:14 +0800

    feat(controller): add list all user method

--- src/main/java/org/cherubic/demo/controller/UserController.java
+++ src/main/java/org/cherubic/demo/controller/UserController.java
@@ -1,8 +1,23 @@
 package org.cherubic.demo.controller;
 
+import org.cherubic.demo.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-
+    
+    private final UserDao userDao;
+    
+    @Autowired
+    public UserController (UserDao userDao) {
+        this.userDao = userDao;
+    }
+    
+    @GetMapping("/hello")
+    public String listAll () {
+        userDao.listAll().forEach(System.out::println);
+        return "hello/index";
+    }
 }
