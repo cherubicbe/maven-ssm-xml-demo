ab587328ff44ef8573308b9304d50557d714ca6f
cutiechi <chi@cutie.ooo>
2018-08-31 16:59:43 +0800

    feat(dao): add list all user method

--- src/main/java/org/cherubic/demo/dao/UserDao.java
+++ src/main/java/org/cherubic/demo/dao/UserDao.java
@@ -1,10 +1,15 @@
 package org.cherubic.demo.dao;
 
+import org.cherubic.demo.entity.User;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 @Transactional
 public interface UserDao {
-
+    
+    List<User> listAll ();
 }
