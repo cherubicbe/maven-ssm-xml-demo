a37b70df41bec9f49b27a22db6ba2cebdfd7e27f
cutiechi <chi@cutie.ooo>
2018-08-31 18:40:39 +0800

    feat(service): add user service impl

---
+++ src/main/java/org/cherubic/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package org.cherubic.demo.service.impl;
+
+import org.cherubic.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+}
