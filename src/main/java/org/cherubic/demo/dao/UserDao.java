8ee41abc81ede8c2a82aba4d7966578b4056f4da
cutiechi <chi@cutie.ooo>
2018-08-31 16:53:21 +0800

    feat(dao): add user dao interface

---
+++ src/main/java/org/cherubic/demo/dao/UserDao.java
@@ -0,0 +1,10 @@
+package org.cherubic.demo.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface UserDao {
+
+}
